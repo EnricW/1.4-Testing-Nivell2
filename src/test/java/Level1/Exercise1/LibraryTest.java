93d6ae3c6a9afbc71b72d87391991f42a4d8231b
Enric <reiben815@gmail.com>
2024-11-20 12:45:19 +0100

    Add BeforeEach code

--- src/test/java/Level1/Exercise1/LibraryTest.java
+++ src/test/java/Level1/Exercise1/LibraryTest.java
@@ -13,25 +13,23 @@ public class LibraryTest {
     @BeforeEach
     public void setup() {
         library = new Library();
+        library.addBook("Book 1");
+        library.addBook("Book 2");
     }
 
     @Test
     public void listIsNotNullAfterInitialization() {
+        library = new Library();
         assertNotNull(library.getBooks());
-        assertEquals(0, library.size());
     }
 
     @Test
     public void addBookIncreasesListSize() {
-        library.addBook("Book 1");
-        library.addBook("Book 2");
         assertEquals(2, library.size());
     }
 
     @Test
     public void bookIsAddedAtSpecificIndex() {
-        library.addBook("Book 1");
-        library.addBook("Book 2");
         assertTrue(library.addBookAtIndex("Book 3", 1));
         assertEquals("Book 3", library.getBookByIndex(1));
     }
@@ -39,47 +37,36 @@ public class LibraryTest {
     @Test
     public void bookIsNotDuplicatedInList() {
         library.addBook("Book 1");
-        library.addBook("Book 1");
-        assertEquals(1, library.size());
-    }
-
-    @Test
-    public void bookIsReturnedFromCorrectIndex() {
-        library.addBook("Book 1");
-        library.addBook("Book 2");
-        library.addBook("Book 3");
-
-        assertEquals("Book 1", library.getBookByIndex(0));
-        assertEquals("Book 2", library.getBookByIndex(1));
-        assertEquals("Book 3", library.getBookByIndex(2));
-    }
-
-    @Test
-    public void addingBookChangesListSize() {
-        assertEquals(0, library.size());
-        library.addBook("Book 1");
-        assertEquals(1, library.size());
-        library.addBook("Book 2");
         assertEquals(2, library.size());
     }
 
     @Test
+    public void bookIsReturnedFromCorrectIndex() {
+        assertEquals("Book 1", library.getBookByIndex(0));
+        assertEquals("Book 2", library.getBookByIndex(1));
+    }
+
+    @Test
+    public void addingBookChangesListSize() {
+        library.addBook("Book 3");
+        assertEquals(3, library.size());
+    }
+
+    @Test
     public void removingBookDecreasesListSize() {
-        library.addBook("Book 1");
-        library.addBook("Book 2");
         assertTrue(library.removeBook("Book 1"));
         assertEquals(1, library.size());
     }
 
     @Test
     public void listIsSortedAfterAddingOrRemovingBook() {
+        library.addBook("Book 4");
         library.addBook("Book 3");
-        library.addBook("Book 1");
-        library.addBook("Book 2");
 
         List<String> books = library.getBooks();
         assertEquals("Book 2", books.get(1));
         assertEquals("Book 1", books.get(0));
+        assertEquals("Book 4", books.get(3));
         assertEquals("Book 3", books.get(2));
 
         library.removeBook("Book 2");
